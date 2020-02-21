bb3fd6bde1c65d837377464f3d882b16ed166f1b
Colin McCulloch <colinmucculloch@gmail.com>
2020-02-21 09:59:05 -0500

    final

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (i = 1; i<6; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute("src", "images/pic"+ i +".jpg");
+    /* this sets the src of the element "newImage" to the proper number */
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(i) {
+        displayedImage.src = i.target.src;
+  }
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function() {
+    let btnClss = btn.getAttribute("class"); 
+    if (btnClss === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "lighten";
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }
+    else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+}
